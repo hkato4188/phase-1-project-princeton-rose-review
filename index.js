43ee28246cb99bde31c86958a87d9184905538c4
Hiroki Kato <hirokikato1@gmail.com>
2023-07-25 22:56:00 -0400

    update add event listeners, add timer, display box and styles

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const baseUrl = "http://localhost:3000"
 const quizUrl = baseUrl + "/quiz"
 const memeUrl = baseUrl + "/memes"
 const quoteUrl = baseUrl + "/quotes"
+const counterElement = document.getElementById("counter");
+const pauseButton = document.getElementById("pause");
 const quizBtn = document.getElementById("quizButton")
 const memeBtn = document.getElementById("laughButton")
 const quoteBtn = document.getElementById("quoteButton")
+const displayImage = document.getElementById("display-image")
+const quoteTextElement = document.getElementById('quote-text')
+const quoteAuthorElement = document.getElementById('quote-author')
+const quizQuestionElement = document.getElementById('quiz-question')
+const quizAnswerElement = document.getElementById('quiz-answer')
+let counterValue = counterElement.innerText * 1;
+let counterIntervalId = 0;
+let timerId;
 
 //Main code
 
@@ -21,66 +31,86 @@ quoteBtn.addEventListener('click', ()=>{
     getQuotes()
 })
 
+addPauseButtonEventListener();
+
+incrementTimer();
+
 // Fetch functions grabbing data from the arrays
 function getQuiz() {
     fetch(quizUrl)
     .then(res => res.json())
-    .then(quiz => {quiz.forEach(giveQuiz)
+    .then(questionArr => {
+        let randomQuestionIndex = generateRandomIndex(questionArr.length)
+        giveQuiz(questionArr, randomQuestionIndex)
+        
     })
 }
 function getMemes(){
     fetch(memeUrl)
     .then(res => res.json())
-    .then(meme => {meme.forEach(giveMeme)
+    .then(memeArr => {
+        let randomMemeIndex = generateRandomIndex(memeArr.length)
+        giveMeme(memeArr, randomMemeIndex)  
     })
 }
 function getQuotes(){
     fetch(quoteUrl)
     .then(res => res.json())
-    .then(quote => {quote.forEach(giveQuote)
+    .then(quoteArr => {
+        let randomQuoteIndex = generateRandomIndex(quoteArr.length)
+        giveQuote(quoteArr, randomQuoteIndex)  
     })
 }
 
 //Function that asks a question in div and then the answer when the div is clicked
-function giveQuiz(quiz){
-    const question = document.createElement('question')
-    question.innerText=quiz.question
-    const answer = document.createElement('answer')
-    let textAnswer = answer.innerText=quiz.answer
-    console.log(textAnswer)
-
-    const boxDiv = document.getElementById("card-display")
-    boxDiv.appendChild(question)
-
-    boxDiv.addEventListener('click', ()=>{
-        alert(textAnswer)
-    })
+function giveQuiz(questionArr, index){
+    
+    quizQuestionElement.innerText = questionArr[index].question
+    
+    
+    setInterval(() => {
+        quizAnswerElement.innerText = `The answer is: ${questionArr[index].answer}`
+    }, 5000)
     
 }
 
 //Function that displays a meme in the div
-function giveMeme(meme){
-    const img = document.createElement('img')
-    img.src = meme.imageUrl
-
-    const boxDiv = document.getElementById("card-display")
-    boxDiv.appendChild(img)
+function giveMeme(memeArr, index){
+    displayImage.src = memeArr[index].imageUrl
 }
 
 //Function that displays a quote and its author when clicked
-function giveQuote(quote){
-    const content = document.createElement('content')
-    const writer = document.createElement('writer')
+function giveQuote(quoteArr, index){
+    let quoteContent = quoteArr[index].quote
+    let author = quoteArr[index].author
+    quoteTextElement.innerText = quoteContent
+    quoteAuthorElement.innerText = author
+
     
-    let textContent = content.innerText=quote.quote
-    let textWriter = writer.innerText=quote.author
-
-    const format = document.createElement ('p')
-    format.innerHTML = textContent + " by " + textWriter
-
-
-    const boxDiv = document.getElementById("card-display")
-    boxDiv.appendChild(format)
 }
 
+function generateRandomIndex(max){
+    return Math.floor(Math.random() * max)
+}
 
+function addPauseButtonEventListener() {
+    pauseButton.addEventListener("click", () => {
+      if (pauseButton.innerText === "pause") {
+        clearInterval(timerId);
+        pauseButton.innerText = "resume";
+        
+      } else {
+        pauseButton.innerText = "pause";
+        
+        incrementTimer();
+      }
+    });
+  }
+
+function incrementTimer() {
+    timerId = setInterval(incrementCounter, 1000);
+  }
+  function incrementCounter() {
+    counterValue += 1;
+    counterElement.innerText = counterValue;
+  }
